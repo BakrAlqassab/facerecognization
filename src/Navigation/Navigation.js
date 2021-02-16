3445c7c2221a1cb1fba8abff2d0a6bd21679bb5a
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2021-02-16 12:28:32 +0200

    clean the code and add navigation bar

---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+    };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+              <h1>hahaha</h1>
+      );
+    }
+
+    
+  }
+}
+
+export default Navigation;
